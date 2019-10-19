a11f285e4a27667fb474164eb2476d7264001d8f
wilsonjvq <wils.jev@gmail.com>
2019-10-19 13:07:42 -0500

    clear codigo

--- application/views/Administrador/listado_autor.php
+++ application/views/Administrador/listado_autor.php
@@ -2,13 +2,13 @@
     <a href='<?= base_url('administrador/add_autor')?>' class='btn btn-primary'>Crear registro</a>
 </div>
 <h2>Autor</h2>
-    <table class='table table-bordered table-dark'>
+<table class='table table-bordered table-dark'>
     <tr class='info'>
-    <th class="text-center">ID</th>
-    <th class="text-center">Nombres</th>
-    <th class="text-center">Apellidos</th>
-    <th class="text-center">Opciones</th>
-</tr>
+        <th class="text-center">ID</th>
+        <th class="text-center">Nombres</th>
+        <th class="text-center">Apellidos</th>
+        <th class="text-center">Opciones</th>
+    </tr>
     <?php if(!empty($Autor)): ?>
     <?php foreach ($Autor as $aut): ?>
     <tr>
@@ -19,19 +19,18 @@
             <a href="<?= base_url('administrador/delete_autor/'.$aut->ID_AUT)?>" class='btn btn-danger eliminar'>Eliminar</a>
             <a href="<?= base_url('administrador/edit_autor/'.$aut->ID_AUT)?>" class='btn btn-success'>Editar</a>
         </td>
-  </tr>
-  <?php endforeach;?>
-  <?php endif; ?>
+    </tr>
+    <?php endforeach;?>
+    <?php endif; ?>
 </table>
 <script>
-  $(document).ready(function(){
-    $('.eliminar').click(function(){
-      
-      $elem = $(this);
-        $.post( $(this).attr('href'), function( data ) {
-          $elem.closest('tr').fadeOut(1000);
+    $(document).ready(function(){
+        $('.eliminar').click(function(){
+            $elem = $(this);
+            $.post( $(this).attr('href'), function( data ) {
+                $elem.closest('tr').fadeOut(1000);
+            });
+            return false;
         });
-      return false;
     });
-  });
 </script>
