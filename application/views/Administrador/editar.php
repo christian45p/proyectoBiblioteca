8dcbcb3a1ea08d8428c6063d3fda5fe5df2ed659
wilsonjvq <wils.jev@gmail.com>
2019-10-19 12:45:22 -0500

    clear codigo

--- application/views/Administrador/editar.php
+++ application/views/Administrador/editar.php
@@ -1,42 +1,42 @@
 <div>
-<h2>Ejemplar # <?= $ejemplar->ejem_id?> </h2>
-<form class="form-horizontal" action="<?= base_url('index.php/administrador/update')?>" method="post">
-    <input type="hidden" name="id" value="<?= $ejemplar->ejem_id?>">
-    <div class="form-group">
-        <label for="titulo" class="col-sm-2 control-label">Titulo</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="titulo" name="titulo" value="<?= $ejemplar->ejem_titulo ?>" placeholder="Nombres">
+    <h2>Ejemplar # <?= $ejemplar->ejem_id?> </h2>
+    <form class="form-horizontal" action="<?= base_url('index.php/administrador/update')?>" method="post">
+        <input type="hidden" name="id" value="<?= $ejemplar->ejem_id?>">
+        <div class="form-group">
+            <label for="titulo" class="col-sm-2 control-label">Titulo</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="titulo" name="titulo" value="<?= $ejemplar->ejem_titulo ?>" placeholder="Nombres">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label for="editorial" class="col-sm-2 control-label">Editorial</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="editorial" name="editorial" value="<?= $ejemplar->ejem_editorial?>" placeholder="Area">
+        <div class="form-group">
+            <label for="editorial" class="col-sm-2 control-label">Editorial</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="editorial" name="editorial" value="<?= $ejemplar->ejem_editorial?>" placeholder="Area">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label for="pagina" class="col-sm-2 control-label">pagina</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="pagina" name="pagina" value="<?= $ejemplar->ejem_paginas?>" placeholder="Login">
+        <div class="form-group">
+            <label for="pagina" class="col-sm-2 control-label">pagina</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="pagina" name="pagina" value="<?= $ejemplar->ejem_paginas?>" placeholder="Login">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label for="isbn" class="col-sm-2 control-label">isbn</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="isbn" name="isbn" value="<?= $ejemplar->ejem_isbn?>" placeholder="Password">
+        <div class="form-group">
+            <label for="isbn" class="col-sm-2 control-label">isbn</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="isbn" name="isbn" value="<?= $ejemplar->ejem_isbn?>" placeholder="Password">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label for="idioma" class="col-sm-2 control-label">idioma</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="idioma" name="idioma" value="<?= $ejemplar->ejem_idioma?>" placeholder="Password">
+        <div class="form-group">
+            <label for="idioma" class="col-sm-2 control-label">idioma</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="idioma" name="idioma" value="<?= $ejemplar->ejem_idioma?>" placeholder="Password">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-primary">Guardar</button>
-            <a href="<?= base_url('index.php/administrador')?>" class="btn btn-danger">Cancelar</a>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-primary">Guardar</button>
+                <a href="<?= base_url('index.php/administrador')?>" class="btn btn-danger">Cancelar</a>
+            </div>
         </div>
-    </div>
-</form>
+    </form>
 </div>
--- application/views/Administrador/editar_usuario.php
+++ application/views/Administrador/editar_usuario.php
@@ -4,58 +4,60 @@
     </div>
     <div class="card-body">
         <form action="" method="post">
+            <div class="form-row">
+                <div class="form-group col-md-3 disabled">
+                    <label for="">Codigo del Estudiante</label>
+                    <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_codigo?>"  id="" disabled>
+                </div>
+            </div>
           <div class="form-row">
-            <div class="form-group col-md-3 disabled">
-              <label for="">Codigo del Estudiante</label>
-              <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_codigo?>"  id="" disabled>
-            </div>
-          </div>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputEmail4">Nombres</label>
-              <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_nombres?>" id="" name="usua_nombres" >
-            </div>
-            <div class="form-group col-md-6">
-              <label for="inputPassword4">Apellidos</label>
-              <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_apellidos?>" id="" name="usua_apellidos" >
-            </div>
+              <div class="form-group col-md-6">
+                  <label for="inputEmail4">Nombres</label>
+                  <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_nombres?>" id="" name="usua_nombres" >
+              </div>
+              <div class="form-group col-md-6">
+                  <label for="inputPassword4">Apellidos</label>
+                  <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_apellidos?>" id="" name="usua_apellidos" >
+              </div>
           </div>
           <div class="form-group">
-            <label for="inputAddress">Direccion</label>
-            <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_direccion?>" id="inputAddress" name="usua_direccion" >
+              <label for="inputAddress">Direccion</label>
+              <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_direccion?>" id="inputAddress" name="usua_direccion" >
           </div>
           <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputEmail4">E-mail</label>
-              <input type="email" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_email?>" id="" name="usua_email" >
-            </div>
-            <div class="form-group col-md-6">
-              <label for="inputPassword4">Telefono</label>
-              <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_telefono?>" id="" name="usua_telefono" >
-            </div>
+              <div class="form-group col-md-6">
+                  <label for="inputEmail4">E-mail</label>
+                  <input type="email" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_email?>" id="" name="usua_email" >
+              </div>
+              <div class="form-group col-md-6">
+                  <label for="inputPassword4">Telefono</label>
+                  <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_telefono?>" id="" name="usua_telefono" >
+              </div>
           </div>
           <div class="form-row">
-            <div class="form-group col-md-3">
-              <label for="inputEmail4">Usuario</label>
-              <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_login?>" id="" disabled="1">
-            </div>
+              <div class="form-group col-md-3">
+                  <label for="inputEmail4">Usuario</label>
+                  <input type="text" class="form-control form-control-sm border border-warning" value="<?= $usuario->usua_login?>" id="" disabled="1">
+              </div>
           </div>
-         <hr class="sidebar-divider my-2">
+          <hr class="sidebar-divider my-2">
           <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputEmail4">Contraseña</label>
-              <input type="email" class="form-control form-control-sm border border-warning" value="" id="" name="usua_email" >
-            </div>
-            <div class="form-group col-md-6">
-              <label for="inputPassword4">Confirmar Contraseña</label>
-              <input type="text" class="form-control form-control-sm border border-warning" value="" id="" name="usua_telefono" >
-            </div>
+              <div class="form-group col-md-6">
+                  <label for="inputEmail4">Contraseña</label>
+                  <input type="email" class="form-control form-control-sm border border-warning" value="" id="" name="usua_email" >
+              </div>
+              <div class="form-group col-md-6">
+                  <label for="inputPassword4">Confirmar Contraseña</label>
+                  <input type="text" class="form-control form-control-sm border border-warning" value="" id="" name="usua_telefono" >
+              </div>
           </div>
-                <!-- Divider -->
-             <hr class="sidebar-divider my-2">
-             <div class="text-center">
-                <a href="<?= base_url('administrador/usuario')?>" class="btn btn-danger mb-2 btn-sm">Cancelar</a>
-                <button type="submit" class="btn btn-primary mb-2 btn-sm">Actualizar Datos</button>
+               
+          <!-- Divider -->
+          
+          <hr class="sidebar-divider my-2">
+              <div class="text-center">
+                  <a href="<?= base_url('administrador/usuario')?>" class="btn btn-danger mb-2 btn-sm">Cancelar</a>
+                  <button type="submit" class="btn btn-primary mb-2 btn-sm">Actualizar Datos</button>
              </div>
         </form>
     </div>
