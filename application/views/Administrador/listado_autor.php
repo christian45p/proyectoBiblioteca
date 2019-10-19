64e8353b62c7bbce28f0ef1dbfd97eef1f7b5688
wilsonjvq <wils.jev@gmail.com>
2019-10-19 13:45:49 -0500

    autor

--- application/views/Administrador/listado_autor.php
+++ application/views/Administrador/listado_autor.php
@@ -1,36 +1,36 @@
-                $elem.closest('tr').fadeOut(1000);
-            $.post( $(this).attr('href'), function( data ) {
-            $elem = $(this);
-            <a href="<?= base_url('administrador/delete_autor/'.$aut->ID_AUT)?>" class='btn btn-danger eliminar'>Eliminar</a>
-            <a href="<?= base_url('administrador/edit_autor/'.$aut->ID_AUT)?>" class='btn btn-success'>Editar</a>
-            return false;
-            });
-        $('.eliminar').click(function(){
-        </td>
-        <td class="text-center"><?= $aut->APELLIDOS_AUT?></td>
-        <td class="text-center"><?= $aut->ID_AUT?></td>
-        <td class="text-center"><?= $aut->NOMBRES_AUT?></td>
-        <td class='text-center'>
-        <th class="text-center">Apellidos</th>
+<div class="text-right container">
+    <a href='<?= base_url('administrador/add_autor')?>' class='btn btn-primary'>Crear registro</a>
+</div>
+<h2>Autor</h2>
+<table class='table table-bordered table-dark'>
+    <tr class='info'>
         <th class="text-center">ID</th>
         <th class="text-center">Nombres</th>
+        <th class="text-center">Apellidos</th>
         <th class="text-center">Opciones</th>
-        });
-    $(document).ready(function(){
     </tr>
+    <?php if(!empty($autor)): ?>
+    <?php foreach ($autor as $aut): ?>
+    <tr>
+        <td class="text-center"><?= $aut->ID_AUT?></td>
+        <td class="text-center"><?= $aut->NOMBRES_AUT?></td>
+        <td class="text-center"><?= $aut->APELLIDOS_AUT?></td>
+        <td class='text-center'>
+            <a href="<?= base_url('administrador/delete_autor/'.$aut->ID_AUT)?>" class='btn btn-danger eliminar'>Eliminar</a>
+            <a href="<?= base_url('administrador/edit_autor/'.$aut->ID_AUT)?>" class='btn btn-success'>Editar</a>
+        </td>
     </tr>
     <?php endforeach;?>
     <?php endif; ?>
-    <?php foreach ($autor as $aut): ?>
-    <?php if(!empty($autor)): ?>
-    <a href='<?= base_url('administrador/add_autor')?>' class='btn btn-primary'>Crear registro</a>
-    <tr class='info'>
-    <tr>
-    });
-</div>
-</script>
 </table>
-<div class="text-right container">
-<h2>Autor</h2>
 <script>
-<table class='table table-bordered table-dark'>
+    $(document).ready(function(){
+        $('.eliminar').click(function(){
+            $elem = $(this);
+            $.post( $(this).attr('href'), function( data ) {
+                $elem.closest('tr').fadeOut(1000);
+            });
+            return false;
+        });
+    });
+</script>
