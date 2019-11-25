19afad4dc38b888de705207413abb6c8d97db6cb
armindnwyr <himkaneki@gmail.com>
2019-11-25 12:55:35 -0500

    Listado con dataTable

--- application/views/Administrador/footer.php
+++ application/views/Administrador/footer.php
@@ -1,33 +1,11 @@
                 </div>
             </div>
-            <!-- End of Content Wrapper -->
-
-        </div>
-        
-        <!-- Page level plugins -->
-        <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/vendor/chart.js/Chart.min.js"></script>
-        <script src="htpps://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" type="text/javascript" charset="utf-8" async defer></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
-        <!-- Page level custom scripts -->
-        <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/js/demo/chart-area-demo.js"></script>
-        <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/js/demo/chart-pie-demo.js"></script>
-        
-        <!-- Bootstrap core JavaScript-->
         <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/vendor/jquery/jquery.min.js"></script>
         <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        
-        <!-- Core plugin JavaScript-->
         <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/vendor/jquery-easing/jquery.easing.min.js"></script>
-        
-        <!-- Custom scripts for all pages-->
         <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/js/sb-admin-2.min.js"></script>
-        
-        <!-- Page level plugins -->
-        <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/vendor/datatables/jquery.dataTables.min.js"></script>
-        <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-        
-        <!-- Page level custom scripts -->
-        <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/js/demo/datatables-demo.js"></script>
+        <script src="<?php echo base_url(); ?>assests/vendor/datatables/jquery.dataTables.min.js"></script>
+        <script src="<?php echo base_url(); ?>assests/vendor/datatables/dataTables.bootstrap4.min.js"></script>
         <script src="<?php echo base_url();?>assests/select2/js/select2.min.js"></script>
         <script type="text/javascript" language="javascript" src="<?php echo base_url();?>assests/js/scripts.js"></script>
     </body>
